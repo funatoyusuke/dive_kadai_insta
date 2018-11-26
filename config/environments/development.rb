6c515ac37d3e4190e38aeb60c0966b3fc87ca624
funatoyusuke <funato1990@yahoo.co.jp>
2018-11-26 02:18:28 +0000

    アセット修正1

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,6 +44,9 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
+  
+  #herokuへのアップ
+  config.serve_static_assets = true
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
